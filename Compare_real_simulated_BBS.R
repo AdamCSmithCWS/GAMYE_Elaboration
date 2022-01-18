aa5b7c1b316bade3398c971ef6f9167e2905eef8
AdamCSmithCWS <adam.smith2@canada.ca>
2022-01-18 09:43:14 -0400

    sim vs real for PIWA show weird observer effects

--- Compare_real_simulated_BBS.R
+++ Compare_real_simulated_BBS.R
@@ -100,17 +100,24 @@ rt_plots[[i]] = ggplot(data = tmp)+
 
 
 # by observer -------------------------------------------------------------
-
+rtmeans <- rt_sum %>% 
+      select(Route,mean) %>% 
+      rename(route_mean = mean)
+    
     obs_sum <- both %>% 
-      group_by(Observer,version) %>% 
+      group_by(Observer,Route,version) %>% 
       summarise(mean = mean(count),
                 sd = sd(count),
                 min = min(count),
-                max = max(count))
+                max = max(count),
+                n_count = n()) %>% 
+      left_join(.,rtmeans) %>% 
+      mutate(cent_mean = mean-route_mean)
     
-    obs_plots <- vector(mode = "list",length = 4)
-    names(obs_plots) <- c("mean","sd","min","max")
-    for(i in names(obs_plots)){
+    obs_plots <- vector(mode = "list",length = 8)
+    names(obs_plots) <- c("cent_mean","mean","sd","min","max",
+                          "mean_by_sd","sd_by_n","mean_by_n")
+    for(i in names(obs_plots)[1:5]){
       rn <- function(x){
         gsub(pattern = i,
              replacement = "tc",
@@ -125,11 +132,60 @@ rt_plots[[i]] = ggplot(data = tmp)+
         labs(title = paste(i,"count by Observer"))+
         facet_wrap(~version,nrow = 2,ncol = 1)
       
+      
+  
     }
-    
-    print(obs_plots[[1]]+ obs_plots[[2]]+ obs_plots[[3]]+ obs_plots[[4]])
-    
 
+    obs_plots[[6]] = ggplot(data = tmp)+
+      geom_point(aes(x = sd,y = mean),
+                 #position = position_jitter(width = 0.1),
+                 alpha = 0.03)+
+      theme_bw()+
+      labs(title = paste("sd by mean count by Observer"))+
+      facet_wrap(~version,nrow = 2,ncol = 1)
+    
+    
+    print(obs_plots[[6]])
+    
+    obs_plots[[7]] = ggplot(data = tmp)+
+      geom_point(aes(y = sd,x = n_count),
+                 #position = position_jitter(width = 0.1),
+                 alpha = 0.03)+
+      theme_bw()+
+      labs(title = paste("sd by n count by Observer"))+
+      facet_wrap(~version,nrow = 2,ncol = 1)
+    
+    
+    print(obs_plots[[7]])
+    
+    obs_plots[[8]] = ggplot(data = tmp)+
+      geom_point(aes(y = mean,x = n_count),
+                 #position = position_jitter(width = 0.1),
+                 alpha = 0.03)+
+      theme_bw()+
+      labs(title = paste("sd by n count by Observer"))+
+      facet_wrap(~version,nrow = 2,ncol = 1)
+    
+    
+    print(obs_plots[[8]])
+    
+    # "mean_by_sd","sd_by_n","mean_by_n"
+    # print(obs_plots[[1]]+ obs_plots[[2]]+ obs_plots[[3]]+ obs_plots[[5]])
+    # 
+
+    
+    
+    pdf("Figures/sim_vs_real.pdf",
+        width = 11,
+        height = 8.5)
+    print(rt_plots[[1]]+ rt_plots[[2]]+ rt_plots[[3]]+ rt_plots[[4]])
+    print(st_plots[[1]]+ st_plots[[2]]+ st_plots[[3]]+ st_plots[[4]])
+    print(stlat_plots[[1]]+ stlat_plots[[2]]+ stlat_plots[[3]]+ stlat_plots[[4]])
+    
+    print(obs_plots[[2]]+ obs_plots[[3]]+ obs_plots[[4]]+ obs_plots[[5]])
+    print(obs_plots[[5]]+ obs_plots[[6]]+ obs_plots[[7]]+ obs_plots[[8]])
+    dev.off()
+    
           
                     
           
