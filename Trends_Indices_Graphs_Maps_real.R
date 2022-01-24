f364beec8572c34ca12a7bbc72c677bc1652b333
AdamCSmithCWS <adam.smith2@canada.ca>
2022-01-24 18:35:31 -0400

    adding mapping

--- Trends_Indices_Graphs_Maps_real.R
+++ Trends_Indices_Graphs_Maps_real.R
@@ -93,10 +93,112 @@ for(i in 1:nrow(fls)){
                scales = "free_y")
     
 print(pl_inds)
+
+tyrs = c(seq(2009,(year_1+10),by = -10),year_1)
+
+stratum_trends <- NULL
+tt_map_data <- vector(mode = "list",length = length(tyrs))
+names(tt_map_data) <- paste0("TY",tyrs)
+tt_map = tt_map_data
+for(yy in tyrs){
+  tt <- trends_function(ind_list = strat_inds_smooth,
+                        start_year = yy) %>% 
+    mutate(species = species,
+           first_year = yy)
   
+ ttmd <- realized_strata_map %>% 
+    left_join(.,tt,by = st_n)
+ tt_map_data[[paste0("TY",yy)]] <- ttmd
+ 
+  
+ ## import one of the bbsBays binned colour scales
+ 
+ ttm  <- ggplot(data = ttmd)+
+   geom_sf(aes(fill = trend))+
+   scale_colour_viridis_c(aesthetics = "fill",
+                          guide = guide_colourbar(title = paste("Trend",yy,"-",2019),
+                                                  ))
+  print(ttm)
+  
+  
+ tt_map[[paste0("TY",yy)]] <- ttm
+ 
+  stratum_trends <- bind_rows(stratum_trends,tt)
+}
+
+
+
   # Overall Annual indices
 
-
+if(dd == "shorebird"){
+  
+  sw_inds <- index_function(fit = stanfit,
+                               parameter = "N",
+                               year_1 = year_1,
+                            strat = NULL,
+                            first_dim = "y")
+  Inds <- sw_inds$indices %>% 
+    mutate(version = "full")
+  
+  sw_inds_smooth <- index_function(fit = stanfit,
+                                      parameter = "nsmooth",
+                                      year_1 = year_1,
+                                      strat = NULL,
+                                      first_dim = "y")
+  
+  Inds_smooth <- sw_inds_smooth$indices %>% 
+    mutate(version = "smooth")
+  
+  Indices_all <- bind_rows(Inds,
+                           Inds_smooth) 
+  
+  
+  
+  pl_Inds <- ggplot(data = Indices_all,aes(x = Year,y = median))+
+    geom_ribbon(aes(ymin = lci,ymax = uci,fill = version),alpha = 0.1)+
+    geom_line(aes(colour = version))+
+    labs(title = species)
+  
+  print(pl_Inds)
+  
+  
+}else{
+ 
+  
+  sw_inds <- index_function(fit = stanfit,
+                               parameter = "n",
+                               year_1 = year_1,
+                               strat = st_n,
+                               weights_df = strat_df,
+                               area = "AREA_1",#"Area",
+                               summary_regions = NULL)
+  Inds <- sw_inds$indices %>% 
+    mutate(version = "full")
+  
+  Inds_smooth <- index_function(fit = stanfit,
+                                      parameter = "nsmooth",
+                                      year_1 = year_1,
+                                      strat = st_n,
+                                      weights_df = strat_df,
+                                      area = "AREA_1",#"Area",
+                                      summary_regions = NULL)
+  Inds_smooth <- Inds_smooth$indices %>% 
+    mutate(version = "smooth")
+  
+  Indices_all <- bind_rows(Inds,
+                           Inds_smooth) 
+  
+  
+  
+  pl_Inds <- ggplot(data = Indices_all,aes(x = Year,y = median))+
+    geom_ribbon(aes(ymin = lci,ymax = uci,fill = version),alpha = 0.1)+
+    geom_line(aes(colour = version))+
+    labs(title = species)
+  
+  print(pl_Inds)
+  
+  
+}
 
 
 
